7163e287e75019934141c8f62c7f7dfed975f90e
Jonathan <jonathan.vigier@outlook.com>
2023-05-06 21:56:52 +0200

    Create function to update the selected dot + closure of the project

--- script.js
+++ script.js
@@ -27,6 +27,8 @@ const dotsContainer = document.querySelector(".dots");
 let n = 0;
 let i = 0;
 
+let direction;
+
 function generateDots() {
   for (let i = 0; i < slides.length; i++) {
     const dot = document.createElement("div");
@@ -36,29 +38,57 @@ function generateDots() {
   dotsContainer.children[0].classList.add("dot_selected");
 }
 
+function updateSelectedDot() {
+  switch (direction) {
+    case "left":
+      if (0 < i < 4) {
+        dotsContainer.children[i].classList.add("dot_selected");
+        dotsContainer.children[i + 1].classList.remove("dot_selected");
+      }
+      break;
+
+    case "right":
+      if (0 < i < 4) {
+        dotsContainer.children[i].classList.add("dot_selected");
+        dotsContainer.children[i - 1].classList.remove("dot_selected");
+      }
+      break;
+
+    default:
+      null;
+      break;
+  }
+}
+
 function switchToLeft() {
   if (n === 0) {
     n = 4;
     i = 4;
+    dotsContainer.children[0].classList.remove("dot_selected");
   }
-
+  direction = "left";
   n -= 1;
   i -= 1;
   imageBanner.src = `./assets/images/slideshow/${slides[n].image}`;
   tagLineBanner.innerHTML = slides[n].tagLine;
+  updateSelectedDot();
 }
 
 function switchToRight() {
   if (n === 3) {
     n = -1;
     i = -1;
+    dotsContainer.children[3].classList.remove("dot_selected");
   }
-
+  direction = "right";
   n += 1;
   i += 1;
   imageBanner.src = `./assets/images/slideshow/${slides[n].image}`;
   tagLineBanner.innerHTML = slides[n].tagLine;
+  updateSelectedDot();
 }
+
+generateDots();
 
 leftArrow.addEventListener("click", switchToLeft);
 
